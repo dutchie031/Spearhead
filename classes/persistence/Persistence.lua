116c0f7e6eab73f3b5555e5703a0646ad4fc27c3
ex61wi <tim.rorije@ing.com>
2024-12-19 09:58:11 +0100

    wip

--- classes/persistence/Persistence.lua
+++ classes/persistence/Persistence.lua
@@ -14,11 +14,30 @@ do
 
     local updateRequired = false
 
+    --[[
+        The Tables: 
+
+        tables {
+            number activeStage;
+
+            Dict stages: [
+                "stageName": {
+                    Dict missions: [
+                        "missionName":{
+                            boolean isCompleted;
+                            Dict aliveUnits: [
+                                "unitName" : true | false
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]]--
+
     local tables = {}
     tables.activeStage = nil
-
     tables.stages = {}
-    
     tables.dead_units = {}
 
     if enabled == true then
@@ -26,15 +45,48 @@ do
 
         -- check if file exists.
 
-        local f = io.open(path, "r")
-        if f == nil then
-            env.warn("No such file found at '" .. path .. "'. Creating it")
-            f = io.open(path, "w")
-            env.info("New file created at: " .. path)
+        local function LoadFromFile()
+            local f = io.open(path, "r")
+            if f == nil then
+                env.warn("No such file found at '" .. path .. "'. Creating it")
+                f = io.open(path, "w")
+                if f == nil then
+                    error("could not create a new file for persistence")
+                end
+                f:write("{}")
+                env.info("New file created at: " .. path)
+            end
+
+            local jsonString = f:read("a")
+            
+            local luaTable = net.json2lua()
+            if f ~= nil then
+                f:close()
+            end
+
+            return luaTable
         end
 
+        local status, result = pcall(LoadFromFile)
 
-        f:close()
+        if status == false then
+            env.error(("[Spearhead][Persistence] " .. Spearhead.Util.toString(result)))
+        else 
+
+            --TODO: Load all. After dumping just to get a better idea of the data structure
+            if result ~= nil then
+
+                if result["activeStage"] ~= nil then
+                    tables.activeStage = tonumber(result["activeStage"])
+                end
+
+                if result["stages"] ~= nil then
+                    for stageZoneName, value in ipairs(t) do
+                        
+                    end
+                end
+            end
+        end
     end
 
     ---Sets the stage in the persistence table
@@ -54,7 +106,9 @@ do
 
     local UpdateContinuous = function(null, time)
 
-        
+        if updateRequired then
+            
+        end
 
         return time + 120
     end
