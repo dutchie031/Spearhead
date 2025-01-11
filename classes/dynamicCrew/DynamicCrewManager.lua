3655dbc87c2e62d9ee90f08df8dc73fbb7c001e6
dutchie032 <dutchie032>
2025-01-11 17:05:06 +0100

    wip

---
+++ classes/dynamicCrew/DynamicCrewManager.lua
@@ -0,0 +1,12 @@
+
+
+local DynamicCrewManager = {}
+do
+    
+
+end
+
+if Spearhead == nil then Spearhead = {} end
+if Spearhead.classes == nil then Spearhead.classes = {} end
+if Spearhead.classes.dynamicCrew == nil then Spearhead.classes.dynamicCrew = {} end
+Spearhead.classes.dynamicCrew.DynamicCrewManager = DynamicCrewManager
